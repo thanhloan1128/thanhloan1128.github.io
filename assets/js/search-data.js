3091e814a96e3cf75cd0d88ed11b7418a867bec2
thanhloan1128 <59511003+thanhloan1128@users.noreply.github.com>
2025-12-15 10:11:58 +0000

    Deploying to gh-pages from @ thanhloan1128/thanhloan1128.github.io@d82f5e4d01f7201fd4c5babbbad8a160a7629ff2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,8 +37,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "nav-repositories",
-          title: "Repositories",
+        },{id: "nav-software",
+          title: "Software",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
           section: "Navigation",
           handler: () => {
