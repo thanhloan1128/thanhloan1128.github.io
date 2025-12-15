1086d5291cc7cc076f0166108a2c40952bf7b392
thanhloan1128 <59511003+thanhloan1128@users.noreply.github.com>
2025-12-15 07:26:51 +0000

    Deploying to gh-pages from @ thanhloan1128/thanhloan1128.github.io@f1ce41a8a0dd35dac48a2a70b1ebaab8a4309b36 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,19 +30,19 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-teaching",
           title: "Teaching",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
+          },
+        },{id: "nav-repositories",
+          title: "Repositories",
+          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/repositories/";
           },
         },{id: "nav-people",
           title: "people",
